e577277b917e988331a85d64440314d31e2385c0
Nikovnik <116634167+nkokic@users.noreply.github.com>
2025-12-17 20:21:16 +0100

    Metabolizing bloodstream (#35071)
    * merged chemical into bloodstream
    
    * changed injectable to bloodstream
    
    * separated bleeding and direct blood removal
    
    * removed blood gain from protein
    
    * reduced blood gain from saline
    
    * rejuvenating fills to reference volume
    
    * fixed blood regulation
    
    * red mead requires stirring to make
    
    * reverted accidental line deletion
    
    * cleared the skeletons from the closet
    
    * additional routing
    
    * field rename for xeno
    
    * removed mention of chemstream and field rename for asteroid mobs
    
    * minor optimizations
    
    * Revert "reduced blood gain from saline"
    
    This reverts commit de26fd1c0d99f3019fe7dd1451a50230cc90f058.
    
    * Revert "removed blood gain from protein"
    
    This reverts commit 7a1648caf39fe26406db73c2a5afa389b82c612f.
    
    * removed unused component fetch
    
    * dead check mini refactor
    
    * eventized blood exclusion
    
    * quick fix
    
    * Pain
    
    * Commit of doom
    
    * COMMIT
    
    * renamed bloodMaxFactor to MaxVolumeFactor
    
    * addressed floating point error
    
    * returned vomiting chemicals
    
    * blood reagent always skips the flush
    
    * no need to mention blood reagent
    
    * fixed passing blood flush
    
    * adadsafasfasfassfasf
    
    * whoops
    
    * merge fixed injectors
    
    * Revert "adadsafasfasfassfasf"
    
    This reverts commit 0a5313a68dd6484d36d28d08930c76851b72ae38.
    
    * simplify reagent removal
    
    * enabled foreign blood transfusion
    
    * Revert "COMMIT"
    
    This reverts commit 19abd679cd7761ebd47bb242bd644176a3006a42.
    
    * simplified reagent removal when modifying blood level
    
    * removed misleading coment since the changes
    
    * documented MetabolismExclusionEvent
    
    * fixed negative negative modification of blood level
    
    * fixed hypervolemia not normalizing
    
    * constrainted blood modification
    
    * returned bloodpack stop on fully healed
    
    * forgot to stage this
    
    * band aid for diona blood
    
    * swapping GetReagent with GetPrototype
    
    * optimize blood filtering
    
    * multiplicative multi reagent blood level calculation
    
    * removed unused stuff
    
    * optimized blood calculation a tiny bit
    
    * added per reagent blood regulation
    
    * optimized (referenceVolume + bloodReagents) into referenceSolution
    
    * polished coded to proper function
    
    * forgot to stage rootable system change
    
    * clean up, unnecessary GetBloodLevel call
    
    * rename method name to TryAddToBloodstream instead of Chemicals
    
    * placed overfill safety
    
    * cleanup and final touches
    
    * final touch
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Server/Body/Systems/MetabolizerSystem.cs
+++ Content.Server/Body/Systems/MetabolizerSystem.cs
@@ -60,6 +60,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using System.Linq;
+using System.Linq;
 using Content.Server.Body.Components;
 using Content.Shared.Administration.Logs;
 using Content.Shared.Bed.Sleep; // Shitmed Change
@@ -200,15 +201,28 @@ public sealed class MetabolizerSystem : SharedMetabolizerSystem
             return;
         }
 
+        // Copy the solution do not edit the original solution list
+        var list = solution.Contents.ToList();
+
+        // Collecting blood reagent for filtering
+        var bloodList = new List<string>();
+        var ev = new MetabolismExclusionEvent(bloodList);
+        RaiseLocalEvent(solutionEntityUid.Value, ref ev);
+
         // randomize the reagent list so we don't have any weird quirks
         // like alphabetical order or insertion order mattering for processing
-        var list = solution.Contents.ToArray();
         _random.Shuffle(list);
+
+        bool isDead = _mobStateSystem.IsDead(solutionEntityUid.Value);
 
         int reagents = 0;
         foreach (var (reagent, quantity) in list)
         {
             if (!_prototypeManager.TryIndex<ReagentPrototype>(reagent.Prototype, out var proto))
+                continue;
+
+            // Skip blood reagents
+            if (bloodList.Contains(reagent.Prototype))
                 continue;
 
             var mostToRemove = FixedPoint2.Zero;
@@ -232,9 +246,9 @@ public sealed class MetabolizerSystem : SharedMetabolizerSystem
                 continue;
 
             // Goob edit start
-            var ev = new ExcludeMetabolismGroupsEvent(ent.Owner);
-            RaiseLocalEvent(solutionEntityUid.Value, ref ev);
-            var exclude = ev.Groups ?? new();
+            var ev2 = new ExcludeMetabolismGroupsEvent(ent.Owner);
+            RaiseLocalEvent(solutionEntityUid.Value, ref ev2);
+            var exclude = ev2.Groups ?? new();
 
             // TODO: Kill MetabolismGroups!
             foreach (var group in ent.Comp1.MetabolismGroups.ExceptBy(exclude, x => x.Id)) // Goob edit end
@@ -257,6 +271,10 @@ public sealed class MetabolizerSystem : SharedMetabolizerSystem
                 // if it's possible for them to be dead, and they are,
                 // then we shouldn't process any effects, but should probably
                 // still remove reagents
+                // todo marty clean this up.
+                if (isDead && !proto.WorksOnTheDead)
+                    continue;
+                // Goob start
                 if (TryComp<MobStateComponent>(solutionEntityUid.Value, out var state))
                 {
                     // Shitmed Change Start
@@ -271,6 +289,7 @@ public sealed class MetabolizerSystem : SharedMetabolizerSystem
 
                     // Shitmed Change End
                 }
+                // Goob end
 
                 var actualEntity = ent.Comp2?.Body ?? solutionEntityUid.Value;
 
