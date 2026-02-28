797af0f0aaf25f6a85b9d5dfea230bd508d55df4
Thesia <lindner.thorben@gmx.de>
2026-02-28 15:43:37 +0100

    save

--- Content.Omu.Server/_BSD/SignalSCI/SignalSystem.cs
+++ Content.Omu.Server/_BSD/SignalSCI/SignalSystem.cs
@@ -21,11 +21,18 @@ public sealed partial class SignalMapSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<SignalMapComponent,SignalDepletionEvent>(CreateSignal);//unsure may be handeled differently
     }
     public override void Update(float frameTime)
     {
-        
+        base.Update(frameTime);
+        var mapQuerry = AllEntityQuery<SignalMapComponent>();
+        while(mapQuerry.MoveNext(out var mapEnt, out var comp))
+        {
+            if(comp.SignalList.Count - comp.DesiredAmountOfSignals <= comp.SignalAmountVariance)continue;
+            while(comp.SignalList.Count < comp.DesiredAmountOfSignals)CreateSignal(comp);
+            int additional = _random.Next(0,comp.SignalAmountVariance);
+            for(int i=0;i<additional;i++)CreateSignal(comp);
+        }
     }
     public void SetupMapSignals(EntityUid uid)//this si called in case the map lacks the component
     {
@@ -33,9 +40,10 @@ public sealed partial class SignalMapSystem : EntitySystem
         EnsureComp<SignalMapComponent>(MapUid);//ensure the map of the station has signals
         return;
     }
-    public void CreateSignal(EntityUid uid,SignalMapComponent signalMapComp,SignalDepletionEvent args)
+    public void CreateSignal(SignalMapComponent signalMapComp)
     {
         TimeSpan disaperanceTime = TimeSpan.FromMinutes(_random.NextFloat(signalMapComp.SignalDurationMin,signalMapComp.SignalDurationMax)) + _gameTiming.RealTime;
+        //MAgic numbers, for the degrees any higher or lower makes no SENCE!!! oddly enought would not braek anything
         Signal newSignal = new Signal(_random.NextFloat(0.0f,360f),_random.NextFloat(signalMapComp.SingalPointsMin,signalMapComp.SingalPointsMax),disaperanceTime);
         signalMapComp.SignalList.Add(newSignal);
     }
