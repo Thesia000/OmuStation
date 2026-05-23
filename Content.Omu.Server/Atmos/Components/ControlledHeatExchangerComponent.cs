272386c9f02510c71e32e78c267b53ee017697ae
Thesia <lindner.thorben@gmx.de>
2026-05-23 22:05:45 +0200

    save

---
+++ Content.Omu.Server/Atmos/Components/ControlledHeatExchangerComponent.cs
@@ -0,0 +1,43 @@
+
+namespace Content.Omu.Server.Atmos.Components;
+
+[RegisterComponent]
+public sealed partial class ControlledHeatExchangerComponent : Component
+{
+
+    [DataField]
+    public string InletNameGasOne { get; set; } = "inlet";
+
+    [DataField]
+    public string OutletNameGasOne { get; set; } = "filter";
+
+    [DataField]
+    public string InletNameGasTwo { get; set; } = "outlet";
+
+    [DataField]
+    public string OutletNameGasTwo { get; set; } = "filterOutlet";
+
+    /// <summary>
+    /// Pipe conductivity (mols/kPa/sec).
+    /// </summary>
+    [DataField("conducvity")]
+    public float G { get; set; } = 1f;
+
+    /// <summary>
+    /// Thermal convection coefficient (J/degK/sec).
+    /// </summary>
+    [DataField("convectionCoefficient")]
+    public float K { get; set; } = 8000f;
+
+    /// <summary>
+    /// Maximal Transfair area the two gases can exchange heat over (m^2)
+    /// </summery>
+    [DataField("maxHeatTransfairArea")]
+    public float A_max { get; set; } = 400f;
+
+    /// <summary>
+    /// Maximal Transfair area the two gases can exchange heat over (m^2)
+    /// </summery>
+    [DataField]
+    public float MaxOutletTemp { get; set; } = Atmospherics.T20C;
+}
