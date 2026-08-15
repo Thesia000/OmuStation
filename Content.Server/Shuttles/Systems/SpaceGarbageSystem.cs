b72f303bdc4f431a1b66cd588961b4f6041b0c71
Khloe Pankewytch <Chrisap97@gmail.com>
2026-08-14 20:00:48 -0400

    Space garbage component fix for steal targets (#1402)
    * init
    
    * Apply suggestion from @NotActuallyMarty
    
    ---------
    
    Co-authored-by: NotActuallyMarty <27968892+NotActuallyMarty@users.noreply.github.com>

--- Content.Server/Shuttles/Systems/SpaceGarbageSystem.cs
+++ Content.Server/Shuttles/Systems/SpaceGarbageSystem.cs
@@ -7,6 +7,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Server.Objectives.Components.Targets;
 using Content.Server.Shuttles.Components;
 using Robust.Shared.Physics;
 using Robust.Shared.Physics.Events;
@@ -31,6 +32,12 @@ public sealed class SpaceGarbageSystem : EntitySystem
     {
         if (args.OtherBody.BodyType != BodyType.Static)
             return;
+
+        // omu todo this is bad and hardcoded, fix parenting on corgi meat later.
+        // Omu Start -- Steal targets like Prime-cut Corgi meat will no longer delete
+        if (HasComp<StealTargetComponent>(uid))
+            return;
+        // Omu End
 
         var ourXform = _xformQuery.GetComponent(uid);
         var otherXform = _xformQuery.GetComponent(args.OtherEntity);
