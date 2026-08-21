a502ff8e515deec75d1bc9ff6573bd87e0810b74
NotActuallyMarty <27968892+NotActuallyMarty@users.noreply.github.com>
2026-08-21 16:22:54 +0200

    NukeSPDX due to it not being very friendly with upstream merges. (#1449)
    NukeSPDX

--- Content.Server/_Mono/CorticalBorer/CorticalBorerInfestedSystem.cs
+++ Content.Server/_Mono/CorticalBorer/CorticalBorerInfestedSystem.cs
@@ -1,6 +1,3 @@
-// SPDX-FileCopyrightText: 2025 Coenx-flex
-// SPDX-FileCopyrightText: 2025 Cojoke
-//
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Shared._Mono.CorticalBorer;
--- Content.Shared/_Funkystation/CCVars/CCVars.Funky.cs
+++ Content.Shared/_Funkystation/CCVars/CCVars.Funky.cs
@@ -1,11 +1,3 @@
-// SPDX-FileCopyrightText: 2025 duston <66768086+dch-GH@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 misghast <51974455+misterghast@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
-// SPDX-FileCopyrightText: 2026 Steve <marlumpy@gmail.com>
-// SPDX-FileCopyrightText: 2026 corresp0nd <46357632+corresp0nd@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2026 marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2026 taydeo <tay@funkystation.org>
-//
 // SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
 
 using Robust.Shared.Configuration;
