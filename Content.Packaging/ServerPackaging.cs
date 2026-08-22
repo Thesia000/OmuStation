c741d06cde4e1fe0a28dc814e207dd5d86a573af
NotActuallyMarty <27968892+NotActuallyMarty@users.noreply.github.com>
2026-08-22 18:01:21 +0200

    Non. Bad maint. Marty, you're supposed to fix the codebase not break it. (#1470)

--- Content.Packaging/ServerPackaging.cs
+++ Content.Packaging/ServerPackaging.cs
@@ -137,7 +137,7 @@ public static class ServerPackaging
             {
                 "publish",
                 "--runtime", runtime,
-                 "--self-contained",
+                "--no-self-contained",
                 "-c", configuration,
                 $"/p:TargetOs={targetOs}",
                 "/p:FullRelease=True",
