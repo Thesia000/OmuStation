6e71169392294f45b57fb2f858591c1c6fd0ad6b
Ishkabelamuda <fish@zcx.pw>
2026-08-24 18:20:54 -0400

    Raise the Fork's Microwave Recipe Debt Limit to 257 (#1530)
    * raise the fork's microwave debt limit to 257
    
    * Updated to 512
    
    ---------
    
    Co-authored-by: Quill <292788791+QuillTheCreature@users.noreply.github.com>
    Co-authored-by: CliveOcelot <165305605+CliveOcelot@users.noreply.github.com>

--- Content.IntegrationTests/Tests/WizdenContentFreeze/WizdenContentFreeze.cs
+++ Content.IntegrationTests/Tests/WizdenContentFreeze/WizdenContentFreeze.cs
@@ -24,7 +24,7 @@ public sealed class WizdenContentFreeze
         var protoMan = server.ProtoMan;
 
         var recipesCount = protoMan.Count<FoodRecipePrototype>();
-        var recipesLimit = 256; // Fucking Kill me Omu
+        var recipesLimit = 512; // Fucking Kill me Omu
 
         if (recipesCount > recipesLimit)
         {
