da3b148e5f72eab3036840a60dc7ed1b77d5fc86
CliveOcelot <165305605+CliveOcelot@users.noreply.github.com>
2026-09-13 21:52:28 +0100

    [FIX] Stop colossus teleport going through walls. (#1722)
    stop people teleporting through walls

--- Content.Server/_DV/CosmicCult/Abilities/Colossus/CosmicSunderSystem.cs
+++ Content.Server/_DV/CosmicCult/Abilities/Colossus/CosmicSunderSystem.cs
@@ -1,5 +1,7 @@
 using Content.Shared._DV.CosmicCult;
 using Content.Shared._DV.CosmicCult.Components;
+using Content.Shared.Interaction;
+using Content.Shared.Physics;
 using Robust.Shared.Timing;
 
 namespace Content.Server._DV.CosmicCult.Abilities;
@@ -9,6 +11,7 @@ public sealed class CosmicSunderSystem : EntitySystem
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly SharedInteractionSystem _interaction = default!;
 
     public override void Initialize()
     {
@@ -19,6 +22,12 @@ public sealed class CosmicSunderSystem : EntitySystem
 
     private void OnColossusSunder(Entity<CosmicColossusComponent> ent, ref EventCosmicColossusSunder args)
     {
+        // Omu start
+        var transform = Transform(args.Performer);
+        if (transform.MapID != _transform.GetMapId(args.Target) || !_interaction.InRangeUnobstructed(args.Performer, args.Target, range: 1000F, collisionMask: CollisionGroup.Opaque, popup: true))
+            return;
+        // Omu end
+
         args.Handled = true;
 
         var comp = ent.Comp;
