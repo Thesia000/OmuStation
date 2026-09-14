e5e6a5f835f52ff36f3fe02179bd756f3cc76743
Thesia <lindner.thorben@gmx.de>
2026-09-14 15:34:27 +0200

    save

--- Content.Server/Mapping/MappingSystem.cs
+++ Content.Server/Mapping/MappingSystem.cs
@@ -76,7 +76,16 @@ public sealed class MappingSystem : EntitySystem
 
             _currentlyAutosaving[uid] = (CalculateNextTime(), name);
             var saveDir = Path.Combine(_cfg.GetCVar(CCVars.AutosaveDirectory), name).Replace(Path.DirectorySeparatorChar, '/');
-            _resMan.UserData.CreateDir(new ResPath(saveDir).ToRootedPath());
+            try//Omu start, prevent crash when autosaving fails due to lack of permissions
+            {
+                _resMan.UserData.CreateDir(new ResPath(saveDir).ToRootedPath());
+            }
+            catch
+            {
+                Log.Error($"Can't create directory: {saveDir}; Insufficient permissions. Autosaves have been deactivated.");
+                _autosaveEnabled = false;
+                break;
+            }//Omu end
 
             var path = new ResPath(Path.Combine(saveDir, $"{DateTime.Now:yyyy-M-dd_HH.mm.ss}-AUTO.yml"));
             Log.Info($"Autosaving map {name} ({uid}) to {path}. Next save in {ReadableTimeLeft(uid)} seconds.");
