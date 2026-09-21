b078e18c66305c9a45256ad79389401842cf5014
harryhulse12-alt <harryhulse12@gmail.com>
2026-09-22 00:36:27 +0100

    Adds the mind role to initial chimera (#1745)
    * Adds the mind role to initial chimera
    
    * guh
    
    * Fixing the letoferol mind role, its annoying me.
    
    * discard

--- Content.Omu.Server/Chimera/ChimeraSystem.cs
+++ Content.Omu.Server/Chimera/ChimeraSystem.cs
@@ -1,12 +1,15 @@
 using Content.Shared.Mind.Components;
 using Content.Server.Roles;
 using Robust.Shared.Prototypes;
+using Content.Shared.Mobs;
+using Content.Server.Mind;
 
 namespace Content.Omu.Server.Chimera;
 
 public sealed class ChimeraSystem : EntitySystem
 {
     [Dependency] private readonly RoleSystem _role = default!;
+    [Dependency] private readonly MindSystem _mind = default!;
 
     private static EntProtoId _chimeraMindRole = "MindRoleChimera";
 
@@ -17,6 +20,7 @@ public sealed class ChimeraSystem : EntitySystem
         SubscribeLocalEvent<ChimeraComponent, MindAddedMessage>(OnMindAdded);
         SubscribeLocalEvent<ChimeraComponent, MindRemovedMessage>(OnMindRemoved);
 
+        SubscribeLocalEvent<ChimeraComponent, MobStateChangedEvent>(OnMobStateChanged);
     }
 
     private void OnMindAdded(Entity<ChimeraComponent> ent, ref MindAddedMessage args)
@@ -27,6 +31,23 @@ public sealed class ChimeraSystem : EntitySystem
 
     private void OnMindRemoved(Entity<ChimeraComponent> ent, ref MindRemovedMessage args)
     {
-        _role.MindRemoveRole<ChimeraComponent>((args.Mind.Owner, args.Mind.Comp));
+        _role.MindRemoveRole<MindRoleChimeraComponent>((args.Mind.Owner, args.Mind.Comp));
+    }
+
+    private void OnMobStateChanged(Entity<ChimeraComponent> ent, ref MobStateChangedEvent args)
+    {
+        if (!_mind.TryGetMind(ent, out var _, out var mind))
+            return;
+
+        if (args.NewMobState != MobState.Dead && !_role.MindHasRole<MindRoleChimeraComponent>(ent))
+        {
+            _role.MindAddRole(ent, _chimeraMindRole, mind: mind);
+            return;
+        }
+
+        if (args.NewMobState != MobState.Alive && _role.MindHasRole<MindRoleChimeraComponent>(ent))
+        {
+            _role.MindRemoveRole<MindRoleChimeraComponent>((ent, mind));
+        }
     }
 }
